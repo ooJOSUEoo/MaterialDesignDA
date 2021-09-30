9ccfe2bd5316d23cff2f744a0ef6043906c63d16
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-09-29 23:15:10 -0500

    material button personalizado

--- app/src/main/java/com/alain/cursos/mdcomponents/ButtonFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/ButtonFragment.java
@@ -8,9 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 
 /**
@@ -21,6 +26,8 @@ public class ButtonFragment extends Fragment {
     public static final String TAG = "Button";
 
     private static Component mInstance;
+
+    Unbinder nUmbinder;
 
     public ButtonFragment() {
         // Required empty public constructor
@@ -38,7 +45,19 @@ public class ButtonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_button, container, false);
+        View view = inflater.inflate(R.layout.fragment_button, container, false);
+        nUmbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @OnClick(R.id.btnEnable)
+    public void onViewClicked(){
+        Toast.makeText(getActivity(), R.string.status_enabled, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        nUmbinder.unbind();
+    }
 }
