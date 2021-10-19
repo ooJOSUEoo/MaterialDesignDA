cf37326f0f434893e30d5bb2387eefba9d52ab75
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-10-19 17:43:36 -0500

    Text Fields - Eventos

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/TextFieldFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/TextFieldFragment.java
@@ -1,6 +1,9 @@
 package com.alain.cursos.mdcomponents.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +13,11 @@ import androidx.fragment.app.Fragment;
 import com.alain.cursos.mdcomponents.R;
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 
@@ -21,6 +28,12 @@ public class TextFieldFragment extends Fragment {
     private static Component mInstance;
 
     Unbinder mUmbinder;
+    @BindView(R.id.etPrice)
+    TextInputEditText etPrice;
+    @BindView(R.id.etCapitalLetter)
+    TextInputEditText etCapitalLetter;
+    @BindView(R.id.tilCapitalLetter)
+    TextInputLayout tilCapitalLetter;
 
     public static Component getmInstance(){
         mInstance = new Component();
@@ -38,6 +51,42 @@ public class TextFieldFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_field, container, false);
+        View view =  inflater.inflate(R.layout.fragment_text_field, container, false);
+        mUmbinder = ButterKnife.bind(this, view);
+
+        tilCapitalLetter.setEndIconOnClickListener(view1 -> {
+            if (etCapitalLetter.getText() != null){
+                String contentStr = etCapitalLetter.getText().toString();
+                etCapitalLetter.setText(contentStr.toUpperCase());
+            }
+        });
+
+        etPrice.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editable.toString().isEmpty() && Integer.valueOf(editable.toString() )<5){
+                    etPrice.setError(getString(R.string.error_price_min));
+                }
+            }
+        });
+
+        return view
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        mUmbinder.unbind();
     }
 }
