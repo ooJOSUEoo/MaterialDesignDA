5e59534eca79e4730a6e9731a8562e04ceeada6a
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-10-19 18:28:34 -0500

    Floating Action Button - Personalización y Eventos

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/FloatingActionButtonFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/FloatingActionButtonFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.alain.cursos.mdcomponents.R;
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class FloatingActionButtonFragment extends Fragment {
@@ -20,6 +26,15 @@ public class FloatingActionButtonFragment extends Fragment {
     private static Component mInstance;
 
     Unbinder mUmbinder;
+    @BindView(R.id.fabDefault)
+    FloatingActionButton fabDefault;
+    @BindView(R.id.fabLegacy)
+    FloatingActionButton fabLegacy;
+    @BindView(R.id.containerMain)
+    ConstraintLayout containerMain;
+    @BindView(R.id.tvLegacy)
+    TextView tvLegacy;
+
 
     public static Component getmInstance(){
         mInstance = new Component();
@@ -37,6 +52,24 @@ public class FloatingActionButtonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_floating_action_button, container, false);
+        View view = inflater.inflate(R.layout.fragment_floating_action_button, container, false);
+        mUmbinder = ButterKnife.bind(this, view);
+
+        fabDefault.setOnClickListener(view1 -> Toast.makeText(getActivity(),R.string.message_action_success,
+                Toast.LENGTH_SHORT).show());
+
+        fabLegacy.setOnClickListener(view1 -> {
+            fabLegacy.setVisibility(View.GONE);
+            tvLegacy.setVisibility(View.GONE);
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        mUmbinder.unbind();
     }
 }
