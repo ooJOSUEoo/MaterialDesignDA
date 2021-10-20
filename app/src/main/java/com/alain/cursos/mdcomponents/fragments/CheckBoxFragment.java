1724f1876be2e2c83e4a4ae35793fa626ef357e4
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-10-19 19:19:58 -0500

    Checkbox Indeterminate

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/CheckBoxFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/CheckBoxFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.alain.cursos.mdcomponents.R;
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+import com.buildware.widget.indeterm.IndeterminateCheckBox;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 
@@ -21,6 +26,10 @@ public class CheckBoxFragment extends Fragment {
     private static Component mInstance;
 
     Unbinder mUmbinder;
+    @BindView(R.id.cbEnable)
+    CheckBox cbEnable;
+    @BindView(R.id.cbEnableIndeterminate)
+    IndeterminateCheckBox cbEnableIndeterminate;
 
     public static Component getmInstance(){
         mInstance = new Component();
@@ -39,8 +48,22 @@ public class CheckBoxFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_check_box, container, false);
+        mUmbinder = ButterKnife.bind(this, view);
 
+        cbEnable.setOnClickListener(view1 -> {
+            String status = cbEnable.isChecked()? "Activo." : "Inactivo";
+            Toast.makeText(getActivity(), status, Toast.LENGTH_SHORT).show();
+
+            cbEnableIndeterminate.setIndeterminate(cbEnable.isChecked());
+        });
 
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        mUmbinder.unbind();
     }
 }
