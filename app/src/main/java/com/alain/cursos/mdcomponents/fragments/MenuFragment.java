019e9406cb87c9397f18f8e970f9655957fd483c
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-10-21 20:57:20 -0500

    Menu_ Dropdown para Autocomplete

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/MenuFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/MenuFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.PopupMenu;
 
 import androidx.fragment.app.Fragment;
@@ -27,6 +29,8 @@ public class MenuFragment extends Fragment {
     Unbinder mUmbinder;
     @BindView(R.id.btnMenuPopup)
     MaterialButton btnMenuPopup;
+    @BindView(R.id.actvCourses)
+    AutoCompleteTextView actvCourses;
 
 
     public static Component getmInstance(){
@@ -54,6 +58,13 @@ public class MenuFragment extends Fragment {
             popupMenu.getMenuInflater().inflate(R.menu.menu_buttom_nav,popupMenu.getMenu());
             popupMenu.show();
         });
+
+        String[] courses = new String[]{"Experto en Firebase para Android +MVP Curso Completo +30Hrs",
+                "Material Desing/Theming Profesional para Android",
+                "Android: Fundamentos apps de calidad",
+                "Koklin 2020"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),R.layout.item_menu_dropdown,courses);
+        actvCourses.setAdapter(adapter);
 
         return view;
     }
