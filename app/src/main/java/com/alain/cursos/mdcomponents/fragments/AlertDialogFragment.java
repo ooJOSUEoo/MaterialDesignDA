286c892daa43bfc5e5b2803999f17e52f5aca0df
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-10-22 17:34:54 -0500

    Dialog_ Opción múltiple y de Confirmación

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/AlertDialogFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/AlertDialogFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -62,12 +64,29 @@ public class AlertDialogFragment extends Fragment {
 
     @OnClick(R.id.btnDialogChooser)
     public void onChooserClicked(){
-
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.select_dialog_item);
+        adapter.add("Opción 1");
+        adapter.add("Opción 2");
+        adapter.add("Opción 3");
+        new MaterialAlertDialogBuilder(getActivity())
+                .setTitle(R.string.dialog_chooser)
+                .setAdapter(adapter, (dialogInterface, i) ->
+                        Toast.makeText(getActivity(), adapter.getItem(i),Toast.LENGTH_SHORT))
+                .show();
     }
 
     @OnClick(R.id.btnDialogConfirm)
     public void onConfirmClicked(){
-
+        //new MaterialAlertDialogBuilder(getActivity())
+        //new MaterialAlertDialogBuilder(getActivity(), R.style.ThemeOverlay_MaterialComponents_Dialog)
+        new MaterialAlertDialogBuilder(getActivity(), R.style.MaterialAlertDialog_MaterialComponents_Title_Icon)
+                .setTitle(R.string.dialog_confirm_title)
+                .setMessage(R.string.card_message_demo_small)
+                .setPositiveButton(R.string.dialog_confirm, (dialogInterface, i) ->
+                        Toast.makeText(getActivity(), R.string.message_action_success, Toast.LENGTH_SHORT).show())
+                .setNegativeButton(R.string.dialog_cancel, null)
+                .show();
     }
 
     @OnClick(R.id.btnDialogFullScreen)
