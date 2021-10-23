ae91c38998d39a27b536747b85bc89ceee3ee59e
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-10-23 11:03:05 -0500

    Picker_ Dialog

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/PickerFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/PickerFragment.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.alain.cursos.mdcomponents.R;
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+import com.google.android.material.datepicker.MaterialDatePicker;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 
@@ -21,6 +26,9 @@ public class PickerFragment extends Fragment {
 
     private static Component mInstance;
     Unbinder mUmbinder;
+
+    @BindView(R.id.containerMain)
+    LinearLayout containerMain;
 
     public static Component getmInstance(){
         mInstance = new Component();
@@ -49,4 +57,17 @@ public class PickerFragment extends Fragment {
         super.onDestroy();
         mUmbinder.unbind();
     }
+    @OnClick({R.id.btnDialog, R.id.btnFullScreen})
+    public void onViewClicked(View view){
+        switch (view.getId()){
+            case R.id.btnDialog:
+                MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+                builder.setTitleText(R.string.picker_title);
+                builder.setTheme(R.style.ThemeOverlay_MaterialComponents_MaterialCalendar);
+                MaterialDatePicker<?> picker = builder.build();
+                picker.show(getFragmentManager(), picker.toString());
+                break;
+            case R.id.btnFullScreen:
+                break;
+        }
 }
